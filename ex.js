4154b8040344e9724e545b4f3f7650081c64902c
letstrie <sakibkhan111296@gmail.com>
2020-11-16 09:36:46 +0600

    Pulling

---
+++ ex.js
@@ -0,0 +1,14 @@
+// const numbers = [-3, 14, -15, 9, 2, -65, 4];
+
+// // numbers.sort((a, b) => (Math.abs(a) % 2) - (Math.abs(b) % 2));
+// numbers.sort((a, b) => a % 2 - b % 2);
+
+// console.log(JSON.stringify(numbers));
+
+const number1 = 15;
+const number2 = 7;
+
+console.log(number1 % number2);
+console.log(-number1 % number2);
+console.log(number1 % -number2);
+console.log(-number1 % -number2);
